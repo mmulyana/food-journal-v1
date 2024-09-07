840b1b7bf99f44daf28ec7efb456535761a7bfd4
mmulyana <mulyan.t20@gmail.com>
2024-09-07 07:21:12 +0700

    refactor: header

--- src/routes/dashboard/component.jsx
+++ src/routes/dashboard/component.jsx
@@ -1,9 +1,19 @@
 import { useState } from 'react'
 import Modal from '../../components/modal'
-import { collection, addDoc, serverTimestamp } from 'firebase/firestore'
+import { collection, addDoc } from 'firebase/firestore'
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 import { db, storage } from '../../utils/firebase'
-import { format } from 'date-fns'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
+import {
+  format,
+  startOfWeek,
+  endOfWeek,
+  addDays,
+  subDays,
+  eachDayOfInterval,
+  isSameDay,
+  isToday,
+} from 'date-fns'
 
 export function AddModal({ fetchItems }) {
   const [isOpen, setIsOpen] = useState(false)
@@ -15,7 +25,7 @@ export function AddModal({ fetchItems }) {
   })
   const [file, setFile] = useState(null)
 
-  const handleInputChange = (e) => {
+  const handleChange = (e) => {
     setNewItem({ ...newItem, [e.target.name]: e.target.value })
   }
 
@@ -83,7 +93,7 @@ export function AddModal({ fetchItems }) {
 
   return (
     <>
-      <div className='py-1 flex justify-center'>
+      <div className='flex justify-center fixed bottom-0 left-0 w-full py-4 bg-gradient-to-t from-[#f5f5f5] to-[#f5f5f5]/10'>
         <button
           className='bg-orange-600 rounded-full px-4 py-1.5 text-white text-sm'
           onClick={() => setIsOpen(true)}
@@ -94,25 +104,29 @@ export function AddModal({ fetchItems }) {
       <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <form onSubmit={handleSubmit} className='space-y-4'>
           <div className='flex flex-col gap-2 mt-4'>
-            <label className='text-sm text-gray-400'>Name</label>
+            <label className='block text-sm font-medium text-gray-700'>
+              Name
+            </label>
             <input
               type='text'
               name='name'
               value={newItem.name}
-              onChange={handleInputChange}
+              onChange={handleChange}
               required
-              className='border px-2.5 py-1.5 rounded text-sm'
+              className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
             />
           </div>
           <div className='flex flex-col gap-2 mt-4'>
-            <label className='text-sm text-gray-400'>Description</label>
+            <label className='block text-sm font-medium text-gray-700'>
+              Description
+            </label>
             <input
               type='text'
               name='description'
               value={newItem.description}
-              onChange={handleInputChange}
+              onChange={handleChange}
               required
-              className='border px-2.5 py-1.5 rounded text-sm'
+              className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
             />
           </div>
 
@@ -128,7 +142,7 @@ export function AddModal({ fetchItems }) {
               id='date'
               name='date'
               value={newItem.date}
-              onChange={handleInputChange}
+              onChange={handleChange}
               className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
             />
           </div>
@@ -145,7 +159,7 @@ export function AddModal({ fetchItems }) {
               id='time'
               name='time'
               value={newItem.time}
-              onChange={handleInputChange}
+              onChange={handleChange}
               className='mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50'
             />
           </div>
@@ -160,5 +174,85 @@ export function AddModal({ fetchItems }) {
         </form>
       </Modal>
     </>
+  )
+}
+
+export function Header({ selectedDate, setSelectedDate }) {
+  const [currentDate, setCurrentDate] = useState(new Date())
+
+  const navigateWeek = (direction) => {
+    setCurrentDate((prevDate) => {
+      const newDate =
+        direction === 'next' ? addDays(prevDate, 7) : subDays(prevDate, 7)
+      setSelectedDate(newDate)
+      return newDate
+    })
+  }
+
+  const selectDay = (day) => {
+    setSelectedDate(day)
+  }
+
+  const weekDays = eachDayOfInterval({
+    start: startOfWeek(currentDate),
+    end: endOfWeek(currentDate),
+  })
+
+  return (
+    <div className='h-fit bg-white mb-7 px-4 pt-4 pb-5 border-b border-gray-200 rounded-b-xl'>
+      <div className='flex justify-between items-center mb-2'>
+        <p className='text-lg font-medium text-orange-600 mb-2.5'>
+          Food Journal
+        </p>
+        <div className='flex gap-2'>
+          <ChevronLeft
+            onClick={() => navigateWeek('prev')}
+            className='cursor-pointer'
+          />
+          <ChevronRight
+            onClick={() => navigateWeek('next')}
+            className='cursor-pointer'
+          />
+        </div>
+      </div>
+      <div className='w-full flex justify-between'>
+        {weekDays.map((day, idx) => (
+          <div
+            key={idx}
+            onClick={() => selectDay(day)}
+            className={[
+              'flex flex-col justify-center items-center px-2.5 rounded py-0.5 cursor-pointer',
+              isSameDay(day, selectedDate) ? 'bg-orange-50' : '',
+              isToday(day) && !isSameDay(day, selectedDate)
+                ? 'border border-orange-300'
+                : '',
+            ].join(' ')}
+          >
+            <p
+              className={[
+                'font-semibold',
+                isSameDay(day, selectedDate) ? 'text-orange-600' : '',
+                isToday(day) && !isSameDay(day, selectedDate)
+                  ? 'text-orange-400'
+                  : '',
+              ].join(' ')}
+            >
+              {format(day, 'd')}
+            </p>
+            <p
+              className={[
+                'text-sm capitalize',
+                isSameDay(day, selectedDate) ? 'text-orange-400' : '',
+                isToday(day) && !isSameDay(day, selectedDate)
+                  ? 'text-orange-300'
+                  : '',
+              ].join(' ')}
+            >
+              {format(day, 'EEE')}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
--- src/routes/dashboard/index.jsx
+++ src/routes/dashboard/index.jsx
@@ -1,26 +1,11 @@
 import { collection, getDocs, orderBy, query, where } from 'firebase/firestore'
-import { itemAtom } from '../../atom/items'
 import { db } from '../../utils/firebase'
 import Card from '../../components/card'
-import { AddModal } from './component'
+import { AddModal, Header } from './component'
 import { useEffect, useState } from 'react'
-import { useAtom } from 'jotai'
-import { ChevronLeft, ChevronRight } from 'lucide-react'
-import {
-  format,
-  startOfWeek,
-  endOfWeek,
-  addDays,
-  subDays,
-  eachDayOfInterval,
-  isSameDay,
-  isToday,
-  startOfDay,
-  endOfDay,
-} from 'date-fns'
+import { startOfDay, endOfDay } from 'date-fns'
 export default function Dashboard() {
-  const [items, setItems] = useAtom(itemAtom)
-  const [currentDate, setCurrentDate] = useState(new Date())
+  const [items, setItems] = useState([])
   const [selectedDate, setSelectedDate] = useState(new Date())
 
   useEffect(() => {
@@ -45,83 +30,10 @@ export default function Dashboard() {
     setItems(itemsList)
   }
 
-  const navigateWeek = (direction) => {
-    setCurrentDate((prevDate) => {
-      const newDate =
-        direction === 'next' ? addDays(prevDate, 7) : subDays(prevDate, 7)
-      setSelectedDate(newDate)
-      return newDate
-    })
-  }
-
-  const selectDay = (day) => {
-    setSelectedDate(day)
-  }
-
-  const weekDays = eachDayOfInterval({
-    start: startOfWeek(currentDate),
-    end: endOfWeek(currentDate),
-  })
-
   return (
     <>
       <div className='max-w-[400px] mx-auto'>
-        <div className='h-fit bg-white mb-7 px-4 pt-4 pb-5 border-b border-gray-200'>
-          <div className='flex justify-between items-center'>
-            <p className='text-base font-medium text-orange-600 mb-2.5'>
-              Food Journal
-            </p>
-            <div className='flex gap-2'>
-              <ChevronLeft
-                onClick={() => navigateWeek('prev')}
-                className='cursor-pointer'
-              />
-              <ChevronRight
-                onClick={() => navigateWeek('next')}
-                className='cursor-pointer'
-              />
-            </div>
-          </div>
-          <div className='w-full flex justify-between'>
-            {weekDays.map((day, idx) => (
-              <div
-                key={idx}
-                onClick={() => selectDay(day)}
-                className={[
-                  'flex flex-col justify-center items-center px-2.5 rounded py-0.5 cursor-pointer',
-                  isSameDay(day, selectedDate) ? 'bg-orange-50' : '',
-                  isToday(day) && !isSameDay(day, selectedDate)
-                    ? 'border border-orange-300'
-                    : '',
-                ].join(' ')}
-              >
-                <p
-                  className={[
-                    'font-semibold',
-                    isSameDay(day, selectedDate) ? 'text-orange-600' : '',
-                    isToday(day) && !isSameDay(day, selectedDate)
-                      ? 'text-orange-400'
-                      : '',
-                  ].join(' ')}
-                >
-                  {format(day, 'd')}
-                </p>
-                <p
-                  className={[
-                    'text-sm capitalize',
-                    isSameDay(day, selectedDate) ? 'text-orange-400' : '',
-                    isToday(day) && !isSameDay(day, selectedDate)
-                      ? 'text-orange-300'
-                      : '',
-                  ].join(' ')}
-                >
-                  {format(day, 'EEE')}
-                </p>
-              </div>
-            ))}
-          </div>
-        </div>
-
+        <Header setSelectedDate={setSelectedDate} selectedDate={selectedDate} />
         {items.map((item) => (
           <Card item={item} key={item.id} />
         ))}
